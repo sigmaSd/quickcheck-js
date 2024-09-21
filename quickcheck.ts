83bb85d095fdcade03f368a41d47f167fd8181bf
Nbiba Bedis <bedisnbiba@gmail.com>
2024-09-21 23:23:20 +0100

    update arbitrary string

--- quickcheck.ts
+++ quickcheck.ts
@@ -101,7 +101,7 @@ const arbitraryString =
       minLength;
     let result = "";
     const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?`~ ";
     for (let i = 0; i < length; i++) {
       result += characters.charAt(
         Math.floor(Math.random() * characters.length),
